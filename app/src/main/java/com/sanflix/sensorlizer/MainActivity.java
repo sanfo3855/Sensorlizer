a77af88a4a7b7cbd3eb6a1970d70db350652e96b
sanfo3855 <sanfelicimatteo@gmail.com>
2017-10-23 18:53:59 +0200

    Table sensors and checkbox

--- app/src/main/java/com/sanflix/sensorlizer/MainActivity.java
+++ app/src/main/java/com/sanflix/sensorlizer/MainActivity.java
@@ -7,7 +7,10 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -29,10 +32,24 @@ public class MainActivity extends Activity implements SensorEventListener {
     private Sensor mTemperature;
 
     private List<String> SensorList;
-    public TextView tvAccellerometer;
+    public TextView tvAccellerometerX;
+    public TextView tvAccellerometerY;
+    public TextView tvAccellerometerZ;
     public TextView tvAmbientTemperature;
-    public TextView tvGravity;
-    public TextView tvGyroscope;
+    public TextView tvGravityX;
+    public TextView tvGravityY;
+    public TextView tvGravityZ;
+    public TextView tvGyroscopeX;
+    public TextView tvGyroscopeY;
+    public TextView tvGyroscopeZ;
+    public TextView tvLigth;
+    public TextView tvLinearAccellerationX;
+    public TextView tvLinearAccellerationY;
+    public TextView tvLinearAccellerationZ;
+    public TextView tvMagneticFieldX;
+    public TextView tvMagneticFieldY;
+    public TextView tvMagneticFieldZ;
+    private String apice2 = "<sup><small>2</small></sup>";
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
@@ -48,10 +65,24 @@ public class MainActivity extends Activity implements SensorEventListener {
         mLinearAccelleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-        tvAccellerometer = (TextView) findViewById(R.id.tvAccellerometerValue);
+        tvAccellerometerX = (TextView) findViewById(R.id.tvAccellerometerValueX);
+        tvAccellerometerY = (TextView) findViewById(R.id.tvAccellerometerValueY);
+        tvAccellerometerZ = (TextView) findViewById(R.id.tvAccellerometerValueZ);
         tvAmbientTemperature = (TextView) findViewById(R.id.tvAmbientTemperatureValue);
-        tvGravity = (TextView) findViewById(R.id.tvGravityValue);
-        tvGyroscope = (TextView) findViewById(R.id.tvGyroscopeValue);
+        tvGravityX = (TextView) findViewById(R.id.tvGravityValueX);
+        tvGravityY = (TextView) findViewById(R.id.tvGravityValueY);
+        tvGravityZ = (TextView) findViewById(R.id.tvGravityValueZ);
+
+        tvGyroscopeX = (TextView) findViewById(R.id.tvGyroscopeValueX);
+        tvGyroscopeY = (TextView) findViewById(R.id.tvGyroscopeValueY);
+        tvGyroscopeZ = (TextView) findViewById(R.id.tvGyroscopeValueZ);
+        tvLigth = (TextView) findViewById(R.id.tvLigthValue);
+        tvLinearAccellerationX = (TextView) findViewById(R.id.tvLinearAccellerationValueX);
+        tvLinearAccellerationY = (TextView) findViewById(R.id.tvLinearAccellerationValueY);
+        tvLinearAccellerationZ = (TextView) findViewById(R.id.tvLinearAccellerationValueZ);
+        tvMagneticFieldX = (TextView) findViewById(R.id.tvMagneticFieldValueX);
+        tvMagneticFieldY = (TextView) findViewById(R.id.tvMagneticFieldValueY);
+        tvMagneticFieldZ = (TextView) findViewById(R.id.tvMagneticFieldValueZ);
     }
 
     @Override
@@ -68,20 +99,45 @@ public class MainActivity extends Activity implements SensorEventListener {
                 float accgx = event.values[0];
                 float accgy = event.values[1];
                 float accgz = event.values[2];
-                tvAccellerometer.setText("GX: " + accgx + " GY: " + accgy + "GZ: " + accgz);
+                tvAccellerometerX.setText(Html.fromHtml("X: " + accgx + "m/s"+apice2));
+                tvAccellerometerY.setText(Html.fromHtml("Y: " + accgy + "m/s"+apice2));
+                tvAccellerometerZ.setText(Html.fromHtml("Z: " + accgz + "m/s"+apice2));
+
             } else if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
                 float temp = event.values[0];
-                tvAmbientTemperature.setText("Temp: " + temp);
+                tvAmbientTemperature.setText("Temp: " + temp + " °C");
             } else if (sensor.getType() == Sensor.TYPE_GRAVITY) {
                 float gragx = event.values[0];
                 float gragy = event.values[1];
                 float gragz = event.values[2];
-                tvGravity.setText("GX: " + gragx + " GY: " + gragy + "GZ: " + gragz);
+                tvGravityX.setText(Html.fromHtml("X: " + gragx + "m/s" + apice2));
+                tvGravityY.setText(Html.fromHtml("Y: " + gragy + "m/s" + apice2));
+                tvGravityZ.setText(Html.fromHtml("Z: " + gragz + "m/s" + apice2));
+
             } else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
                 float gyrx = event.values[0];
                 float gyry = event.values[1];
                 float gyrz = event.values[2];
-                tvGyroscope.setText("GX: " + gyrx + " GY: " + gyry + "GZ: " + gyrz);
+                tvGyroscopeX.setText("X: " + gyrx + "rad/s");
+                tvGyroscopeY.setText("Y: " + gyry + "rad/s");
+                tvGyroscopeZ.setText("Z: " + gyrz + "rad/s");
+            } else if(sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
+                float lax = event.values[0];
+                float lay = event.values[1];
+                float laz = event.values[2];
+                tvLinearAccellerationX.setText(Html.fromHtml("X: " + lax + "m/s" + apice2));
+                tvLinearAccellerationX.setText(Html.fromHtml("Y: " + lay + "m/s" + apice2));
+                tvLinearAccellerationX.setText(Html.fromHtml("Z: " + laz + "m/s" + apice2));
+            } else if(sensor.getType() == Sensor.TYPE_LIGHT){
+                float ligth = event.values[0];
+                tvLigth.setText("Light Level: " + ligth + "lx");
+            } else if(sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+                float mfx = event.values[0];
+                float mfy = event.values[1];
+                float mfz = event.values[2];
+                tvMagneticFieldX.setText("X: " + mfx + "μT");
+                tvMagneticFieldY.setText("Y: " + mfy + "μT");
+                tvMagneticFieldZ.setText("Z: " + mfz + "μT");
             }
     }
 
